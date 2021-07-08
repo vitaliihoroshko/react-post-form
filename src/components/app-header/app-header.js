ac79c079d728f25a0edef9da4a55005b67eb464e
vitaliihoroshko <vitaliihoroshko@gmail.com>
2021-07-08 23:21:55 +0300

    Modify and add files

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './app-header.css';
+import styled from 'styled-components';
+
+const Header = styled.div `
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    h1 {
+        font-size: 26px;
+    }
+    h2 {
+        font-size: 1.2rem;
+        color: grey;
+    }
+`
+
+const AppHeader = ({liked, allPosts}) => {
+    return (
+        <Header>
+            <h1>Vitalii Horoshko</h1>
+            <h2>Записів - {allPosts}, з них сподобалося - {liked}</h2>
+        </Header>
+    )
+}
+
+export default AppHeader;
